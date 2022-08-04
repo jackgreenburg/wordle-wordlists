106a589508c8cf00e6211f54699b2b286d4be288
jackgreenburg <jgreenburg@middlebury.edu>
2022-08-04 16:09:16 -0400

    second commit

--- wordle_compare.py
+++ wordle_compare.py
@@ -6,13 +6,13 @@ def format_list(list_as_string: str) -> List[str]:
     return set(re.findall(word_regex, list_as_string))
 
 
-def get_wordlist(filename: str, answers_prefix:str = "mo", word_list_prefix:str = "fo") -> Tuple[List[str], List[str]]:
+def get_wordlist(filename: str, answers_var_name: str, word_list_var_name: str) -> Tuple[List[str], List[str]]:
     with open(filename, "r", encoding="cp437") as f:
         text = f.read()
-    answers_regex = fr"{answers_prefix}=\[(.*?)\]"
+    answers_regex = fr"{answers_var_name}=\[(.*?)\]"
     answers = re.search(answers_regex, text).group()
 
-    word_list_regex = fr"{word_list_prefix}=\[(.*?)\]"
+    word_list_regex = fr"{word_list_var_name}=\[(.*?)\]"
     word_list = re.search(word_list_regex, text).group()
     return format_list(answers), format_list(word_list)
 
@@ -26,29 +26,38 @@ def get_wordlists(filename: str) -> Tuple[List[str], List[str]]:
     print(answers[0][0])
     return format_list(answers[0][0]), format_list(answers[1][0])
 
+def save_wordlist(filename: str, wordlist: List[str]):
+    with open(filename, 'w') as f:
+        for word in wordlist:
+            f.write(f"{word}\n")
+
+
 def main():
-    answers_old, full_list_old = get_wordlist("jan_01_wordle_snapshot.js", "Aa", "La")
+    answers_old, guessable_old = get_wordlist("jan_01_wordle_snapshot.js", "Aa", "La")
     # answers_new, w = get_wordlist("apr_18_wordle_snapshot.js", "mo", "fo")
     # answers_new, w = get_wordlist("may_09_wordle_snapshot.js", "ko", "wo")
-    answers_new, full_list_new = get_wordlist("aug_08_wordle_snapshot.js", "he", "be")
+    answers_new, guessable_new = get_wordlist("aug_08_wordle_snapshot.js", "he", "be")
 
     print("old wordlist # answers:", len(answers_old))
     print("new wordlist # answers:", len(answers_new))
+    # save_wordlist("answers_old.txt", answers_old)
+    # save_wordlist("answers_new.txt", answers_new)
 
     added = [answer for answer in answers_new if answer not in answers_old]
     print("answers added:", added)
     removed = [answer for answer in answers_old if answer not in answers_new]
     print("answers removed:", removed)
 
-    print("old wordlist # guesses:", len(full_list_old))
-    print("new wordlist # guesses:", len(full_list_new))
 
-    added = [answer for answer in full_list_new if answer not in full_list_old]
+    print("old wordlist # guesses:", len(guessable_old))
+    print("new wordlist # guesses:", len(guessable_new))
+    # save_wordlist("guessable_old.txt", guessable_old)
+    # save_wordlist("guessable_new.txt", guessable_new)
+
+    added = [answer for answer in guessable_new if answer not in guessable_old]
     print("guesses added:", added)
-    removed = [answer for answer in full_list_old if answer not in full_list_new]
+    removed = [answer for answer in guessable_old if answer not in guessable_new]
     print("guesses removed:", removed)
-
-    return True
 
 if __name__ == "__main__":
     main()
